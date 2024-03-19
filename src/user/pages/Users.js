bb806f7d4df80b22a02480d71d7947363604d04c
Kerwin Ejorango <lionelejorango@gmail.com>
2024-03-19 10:24:35 +0800

    feat(Users): Implemented shared components for Users components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -7,7 +7,8 @@ const Users = () => {
     {
       id: "u1",
       name: "John Smith",
-      image: "https://www.freepik.com/free-photos-vectors/profile-pic",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       places: 4,
     },
   ];
