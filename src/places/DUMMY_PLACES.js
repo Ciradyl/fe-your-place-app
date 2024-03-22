6d88634047de2905824da5810259ad74dbc1942b
Kerwin Ejorango <lionelejorango@gmail.com>
2024-03-22 10:34:38 +0800

    refactor: Created a dummy file for List of Users and Users places

---
+++ src/places/DUMMY_PLACES.js
@@ -0,0 +1,28 @@
+export const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "World Trade Center",
+    description: "One of the most famous sky scrappers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c5/World_Trade_Center%2C_New_York_City_-_aerial_view_%28March_2001%29.jpg",
+    address: "Manhattan, New York, NY 10001",
+    coordinates: {
+      lat: 40.7127,
+      lng: 74.0134,
+    },
+    creatorId: "u1",
+  },
+  {
+    id: "p2",
+    title: "World Trade Center",
+    description: "One of the most famous sky scrappers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c5/World_Trade_Center%2C_New_York_City_-_aerial_view_%28March_2001%29.jpg",
+    address: "Manhattan, New York, NY 10001",
+    coordinates: {
+      lat: 40.7127,
+      lng: 74.0134,
+    },
+    creatorId: "u2",
+  },
+];
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,21 +1,12 @@
 import React from "react";
 
 import UsersList from "../components/UsersList";
+import { DUMMY_USERS } from "../DUMMY_USERS";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "John Smith",
-      image:
-        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      places: 4,
-    },
-  ];
-
   return (
     <>
-      <UsersList items={USERS} />
+      <UsersList items={DUMMY_USERS} />
     </>
   );
 };
