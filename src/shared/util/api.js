be488df6c9990c8c371e5be9682ac2766a20b23a
Kerwin Ejorango <lionelejorango@gmail.com>
2024-04-26 20:48:34 +0800

    fix: added get request handler for user

--- src/shared/util/api.js
+++ src/shared/util/api.js
@@ -1,6 +1,7 @@
-const YOUR_PLACE_BASE_URL = 'http://localhost:5000';
+const YOUR_PLACE_BASE_URL = "http://localhost:5000";
 
 export const YOUR_PLACE_API_URLS = {
-    LOGIN: `${YOUR_PLACE_BASE_URL}/api/users/login`,
-    SIGNUP: `${YOUR_PLACE_BASE_URL}/api/users/signup`,
-}
+  LOGIN: `${YOUR_PLACE_BASE_URL}/api/users/login`,
+  SIGNUP: `${YOUR_PLACE_BASE_URL}/api/users/signup`,
+  USERS: `${YOUR_PLACE_BASE_URL}/api/users`,
+};
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { UsersList } from "../__index__";
+import {
+  ErrorModal,
+  LoadingSpinner,
+} from "../../shared/components/UIElements/__index__";
+import { YOUR_PLACE_API_URLS } from "../../shared/util/api";
 import { DUMMY_USERS } from "../DUMMY_USERS";
 
 const Users = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorOccured, setErrorOccured] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    const fetchRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(YOUR_PLACE_API_URLS.USERS);
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+
+        setLoadedUsers(data.users);
+      } catch (e) {
+        setErrorOccured(e.message);
+      }
+      setIsLoading(false);
+    };
+    fetchRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setErrorOccured(null);
+  };
   return (
     <>
-      <UsersList items={DUMMY_USERS} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+      <ErrorModal error={errorOccured} onClear={errorHandler} />
     </>
   );
 };
