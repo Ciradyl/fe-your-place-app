99fa84de78d3a490b499614bcc8a93f894e70b1c
Kerwin Ejorango <lionelejorango@gmail.com>
2024-05-01 22:49:13 +0800

    fix: implemented backend logic to creating post

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
 import { Button, Input } from "../../shared/components/FormElements/__index__";
+import {
+  ErrorModal,
+  LoadingSpinner,
+} from "../../shared/components/UIElements/__index__";
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
 import "./PlaceForm.css";
-
+import { useHttpClient } from "../../shared/hooks/http-hooks";
 import { useForm } from "../../shared/hooks/form-hook";
+import { AUTHENTICATION_CONTEXT } from "../../shared/context/auth-context";
+import { YOUR_PLACE_API_URLS } from "../../shared/util/api";
 
 const NewPlace = () => {
+  const authContext = useContext(AUTHENTICATION_CONTEXT);
+  const { isLoading, errorOccured, sendRequest, clearError } = useHttpClient();
   const [formState, inputHandler] = useForm(
     {
       title: {
@@ -28,42 +37,61 @@ const NewPlace = () => {
     false
   );
 
-  const placeSubmitHandler = (e) => {
+  const placeHistory = useHistory()
+
+  const placeSubmitHandler = async (e) => {
     e.preventDefault();
-    console.log("Form Submitted", formState.inputs);
+    try {
+      await sendRequest(
+        YOUR_PLACE_API_URLS.PLACES,
+        "POST",
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+          address: formState.inputs.address.value,
+          creatorId: authContext.userId,
+        }),
+        { "Content-Type": "application/json" }
+      );
+      placeHistory.push(YOUR_PLACE_API_URLS.HOME)
+    } catch (e) {}
   };
 
   return (
-    <form className="place-form" onSubmit={placeSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
-        onInput={inputHandler}
-      />
-      <Input
-        id="address"
-        element="input"
-        label="Address"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid address."
-        onInput={inputHandler}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter an appropriate description (at least 5 characters)."
-        onInput={inputHandler}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        Add Place
-      </Button>
-    </form>
+    <>
+      <ErrorModal error={errorOccured} onClear={clearError} />
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title."
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          element="input"
+          label="Address"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid address."
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter an appropriate description (at least 5 characters)."
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          Add Place
+        </Button>
+      </form>
+    </>
   );
 };
 
