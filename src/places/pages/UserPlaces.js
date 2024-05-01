17ae274b749a7e520a9ee31b7fff2a861e2e1336
Kerwin Ejorango <lionelejorango@gmail.com>
2024-05-01 23:21:34 +0800

    fix: implement backend connectivity to getting the places of a user

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
-import { DUMMY_PLACES } from "../DUMMY_PLACES";
-
+import {
+  ErrorModal,
+  LoadingSpinner,
+} from "../../shared/components/UIElements/__index__";
+import { YOUR_PLACE_API_URLS } from "../../shared/util/api";
+import { useHttpClient } from "../../shared/hooks/http-hooks";
+// import { DUMMY_PLACES } from "../DUMMY_PLACES";
 
 const UserPlaces = () => {
-  const USER_ID = useParams().userId;
-  const LOADED_PLACES = DUMMY_PLACES.filter(
-    (place) => place.creatorId === USER_ID
-  );
+  const { isLoading, errorOccured, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState();
 
-  return <PlaceList items={LOADED_PLACES} />;
+  const USER_ID = useParams().userId;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const response = await sendRequest(
+          YOUR_PLACE_API_URLS.PLACES_GET_BY_USER_ID + USER_ID
+        );
+        setLoadedPlaces(response.places);
+      } catch (e) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, USER_ID]);
+
+  return (
+    <>
+      <ErrorModal error={errorOccured} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </>
+  );
 };
 
 export default UserPlaces;
