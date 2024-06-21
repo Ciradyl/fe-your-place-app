79a7cb5a3af3ed9d8519d516dc525407eda98dac
Kerwin Ejorango <lionelejorango@gmail.com>
2024-06-21 20:49:32 +0800

    feat: use token and expiration for auto authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -11,19 +11,61 @@ import { Auth, Users } from "./user/__index__";
 import { MainNavigation } from "./shared/components/Navigation/__index__";
 import { AUTHENTICATION_CONTEXT } from "./shared/context/auth-context";
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState();
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, existingExpirationDate) => {
     setToken(token);
     setUserId(uid);
+
+    const authTokenExpirationDate =
+      existingExpirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(authTokenExpirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiraion: authTokenExpirationDate.toISOString(),
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
+    localStorage.removeItem("userData");
   }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiraion > new Date())
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiraion)
+      );
+    }
+  }, [login]);
 
   let APP_ROUTES;
   if (token) {
