6625b5ae17c0d4a5bdf3b495f356c0b2895d4fdc
Kerwin Ejorango <lionelejorango@gmail.com>
2024-06-24 22:54:10 +0800

    fix: removed creatorid from form since backend handles it using token

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -54,7 +54,6 @@ const NewPlace = () => {
       formData.append("title", formState.inputs.title.value);
       formData.append("description", formState.inputs.description.value);
       formData.append("address", formState.inputs.address.value);
-      formData.append("creatorId", authContext.userId);
       formData.append("image", formState.inputs.image.value);
       
       await sendRequest(YOUR_PLACE_API_URLS.PLACES, "POST", formData, {
